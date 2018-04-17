5cbd8e3c35655f6fb760918da6f06c00a87fafc8
Jarred Binder <jarredbinder@gmail.com>
2018-04-16 21:37:16 -0500

    Add files via upload

--- Dictionary.cpp
+++ Dictionary.cpp
@@ -2,190 +2,163 @@
 
 #include "Dictionary.h"
 
-int max(int x, int y)
-{
-  if (x > y)
-    return x;
-  else
-    return y;
+#include <queue>
+
+#define MIN_CAPACITY 11
+int capacity = MIN_CAPACITY;
+
+enum EntryType {Legitimate, Empty, Deleted};
+
+struct HashNode{
+    string data;
+    enum EntryType info;
+};
+
+struct HashTable{
+    int capacity;
+    int size;
+    HashNode *table;
+};
+
+HashTable *htable;
+
+HashTable * initializeTable(int capacity){
+    if(capacity < MIN_CAPACITY)
+        return NULL;
+    htable = new HashTable;
+    if(htable == NULL)
+        return NULL;
+    htable->capacity = capacity;
+    htable->table = new HashNode[htable->capacity];
+    if(htable->table == NULL)
+        return NULL;
+    for(int i = 0; i < htable->capacity; i++){
+        htable->table[i].info = Empty;
+        htable->table[i].data = "";
+    }
+    htable->size = 0;
+    cout<<"htable size initialized to size: "<<htable->size<<" and capacity: "<<htable->capacity<<endl;
+    return htable;
 }
 
-Dictionary::Dictionary()
-{
-  root = NULL;
-  size = 0;
+Dictionary::Dictionary(){
+//    size = MIN_CAPACITY;    // size = size of hashTable array
+//    AVLTreeNode hashTable [size]; 
+//    root = hashTable;       // Pointer to first element of array.
+//                            // To access following elements use *(root + n)
+    initializeTable(MIN_CAPACITY);
 }
 
-Dictionary::Dictionary(const Dictionary& orig)
-{
+Dictionary::Dictionary(const Dictionary& orig){
   this->copyDict(orig);
 }
 
-Dictionary::~Dictionary()
-{
+Dictionary::~Dictionary(){
   this->deleteDict();
 }
 
-Dictionary& Dictionary::operator=(const Dictionary& orig)
-{
-  if (this->root != orig.root)
-    {
-      this->deleteDict();
-      this->copyDict(orig);
+Dictionary& Dictionary::operator=(const Dictionary& orig){
+    this->copyDict(orig);
+}
+
+unsigned int hashFunction(const string & key, int tableSize){
+    unsigned int hashVal = 0;
+    
+    for(char ch : key){
+        hashVal = 37 * hashVal + ch;
     }
-  return *this;
+    unsigned int ans = hashVal % tableSize;
+    cout<<"hash function = "<<ans<<endl;
+    return ans;
 }
 
-int GetNodeHeight(AVLTreeNode* t)
-{
-  return t == NULL ? -1 : t->height;
-}
-
-void RotateWithLeftChild(AVLTreeNode*& k2)
-{
-  AVLTreeNode* k1 = k2->left;
-  k2->left = k1->right;
-  k1->right = k2;
-  k2->height = max(GetNodeHeight(k2->left), GetNodeHeight(k2->right)) + 1;
-  k1->height = max(GetNodeHeight(k1->left), k2->height) + 1;
-  k2 = k1;
-}
-
-void RotateWithRightChild(AVLTreeNode*& k2)
-{
-  AVLTreeNode* k1 = k2->right;
-  k2->right = k1->left;
-  k1->left = k2;
-  k2->height = max(GetNodeHeight(k2->left), GetNodeHeight(k2->right)) + 1;
-  k1->height = max(k2->height, GetNodeHeight(k1->left)) + 1;
-  k2 = k1;
-}
-
-void DoubleWithLeftChild(AVLTreeNode*& k3)
-{
-  RotateWithRightChild(k3->left);
-  RotateWithLeftChild(k3);
-}
-
-void DoubleWithRightChild(AVLTreeNode*& k3)
-{
-  RotateWithLeftChild(k3->right);
-  RotateWithRightChild(k3);
-}
-
-void Insert(AVLTreeNode* p, AVLTreeNode*& t)
-{
-
-  if (t == NULL)
-    t = p;
-  else
-    {
-      if (p->data < t->data) // headed left
-	{
-	  Insert(p, t->left);
-	  if ( GetNodeHeight(t->left) - GetNodeHeight(t->right) == 2)
-	    if (p->data < t->left->data)
-	      RotateWithLeftChild(t);
-	    else
-	      DoubleWithLeftChild(t);
-	}
-      else
-	{
-	  Insert(p, t->right);
-	  if (GetNodeHeight(t->right) - GetNodeHeight(t->left) == 2)
-	    if (p->data > t->right->data)
-	      RotateWithRightChild(t);
-	    else
-	      DoubleWithRightChild(t);
-	}
+bool isPrime(int n){
+    if(n == 2 || n == 3)
+        return true;
+    if(n % 2 == 0 || n % 3 == 0)
+        return false;
+    int divisor = 6;
+    while(divisor * divisor - 2 * divisor + 1 <= n){
+        if(n % (divisor - 1) == 0)
+            return false;
+        if(n % (divisor + 1) == 0)
+            return false;
+        divisor += 6;
     }
-  t->height =max(GetNodeHeight(t->left), GetNodeHeight(t->right)) + 1;
+    return true;
 }
 
-void Dictionary::AddEntry(string anEntry)
-{
-  // create the node
-  AVLTreeNode* p = new AVLTreeNode;
-  p->data = anEntry;
-  p->height = 0;
-  p->left = NULL;
-  p->right = NULL;
+int nextPrime(int prime){
+    prime *= 2;
+    while(!isPrime(++prime)){}
+    return prime;
+}
+
+HashTable * Rehash(HashTable * htable);
+
+void Insert(string anEntry){
+    int collisions = 0;
+    bool added = false;
+    while(!added){
+        unsigned int temp = hashFunction(anEntry, htable->capacity);
+        int pos = (temp + collisions * collisions) % htable->capacity;
+        cout<<"pos: "<<pos<<endl;
+        if(htable->table[pos].info == Legitimate){
+            collisions++;
+        }else{
+            htable->table[pos].info = Legitimate;
+            htable->table[pos].data = anEntry;
+            added = true;
+        }
+    }
+    htable->size = htable->size + 1;
+    cout<<"htable size increased to: "<<htable->size<<endl;
+}
+
+void Dictionary::AddEntry(string anEntry){
+    double tempSize = htable->size;
+    double tempCapacity = htable->capacity;
+    double load = tempSize / tempCapacity;
+    cout<<"Load: "<<load<<endl;
+    if(load >= .5)
+        Rehash(htable);
+    
+    Insert(anEntry);
+}
+
+HashTable * Rehash(HashTable * htable){
+    cout<<"Rehash called."<<endl;
+    HashNode * table = htable->table;
+    cout<<"Next prime: "<<nextPrime(htable->capacity)<<endl;
+    htable = initializeTable(nextPrime(htable->capacity));
+    for(int i = 0; i < htable->capacity; i++){
+        if(table[i].info == Legitimate)
+            Insert(table[i].data);
+    }
+    cout<<"Rehash finished. New table cap: "<<htable->capacity<<endl;
+}
+
+bool Dictionary::FindEntry(string key){
+//    int pos = hash(key, htable->capacity);
+//    if(htable->table[pos].info = Legitimate && htable->table[pos].data == key)
+//        return true;
+//    return false;
+    for(int i = 0; i < htable->capacity; i++){
+        if(htable->table[i].info == Legitimate && htable->table[i].data == key)
+            return true;
+    }
+    return false;
+}
+
+void Dictionary::PrintSorted(ostream& outStream){
   
-  Insert(p, root);
 }
 
-bool Dictionary::FindEntry(string key)
-{
-  AVLTreeNode* q = root;
-  bool found = false;
-
-  while (!found && q)
-    {
-      if (q->data == key)
-	{
-	  found = true;
-	}
-      else if (q->data < key)
-	{
-	  q = q->right;
-	}
-      else
-	{
-	  q = q->left;
-	}
-    }
-  return found;
-}
-	
-void PrintInOrder(AVLTreeNode* p, ostream& outStream)
-{
-  if (p)
-    {
-      PrintInOrder(p->left, outStream);
-      outStream << p->data << endl;
-      PrintInOrder(p->right, outStream);
-    }
+void Dictionary::copyDict(const Dictionary& orig){
+  
 }
 
-void Dictionary::PrintSorted(ostream& outStream)
-{
-  PrintInOrder(root,outStream);
-}
-
-AVLTreeNode* copyTree(const AVLTreeNode* node)
-{
-  if (!node)
-    return NULL;
-  else
-    {
-      AVLTreeNode* newNode = new AVLTreeNode;
-      newNode->data = node->data;
-      newNode->height = node->height;
-      newNode->left = copyTree(node->left);
-      newNode->right = copyTree(node->right);
-      return newNode;
-    }
-}
-
-void Dictionary::copyDict(const Dictionary& orig)
-{
-  this->size = orig.size;
-  this->root = copyTree(orig.root);
-}
-
-void deleteTree(AVLTreeNode* node)
-{
-  if (node)
-    {
-      deleteTree(node->left);
-      deleteTree(node->right);
-      delete node;
-    }
-}
-
-void Dictionary:: deleteDict()
-{
-  deleteTree(this->root);
-  this->root = NULL;
+void Dictionary:: deleteDict(){
+    
 }
 
